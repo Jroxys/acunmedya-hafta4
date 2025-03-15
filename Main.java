50a074ea2d81380f999e98a0ae565c70479afba1
Your Name <you@example.com>
2025-03-15 17:32:37 +0300

    ODEV

--- Main.java
+++ Main.java
@@ -8,6 +8,8 @@ import Intro.odevhafta4.repository.concrete.inmemory.InMemoryTechnologyRepositor
 import Intro.odevhafta4.service.concrete.ProgramingLanguageImpl;
 import Intro.odevhafta4.service.concrete.TechnologyImpl;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -18,23 +20,34 @@ public class Main {
         ProgramingLanguageImpl programmingLanguageService = new ProgramingLanguageImpl(programmingLanguageRepository);
         TechnologyImpl technologyService = new TechnologyImpl(technologyRepository);
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a programming language name:");
+        String languageName = scanner.nextLine();
+        System.out.println("Please enter id: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Programimng Language: " + languageName + " Id: " + id);
+        System.out.println("Please enter a technology name:");
+        String technologyName = scanner.nextLine();
+        System.out.println("Please enter id: ");
+        int id2 = scanner.nextInt();
+        System.out.println("Technology: " + technologyName + " Id: " + id2);
 
-        ProgrammingLanguage java = new ProgrammingLanguage(1, "Java");
-        programmingLanguageService.add(java);
+        ProgrammingLanguage programmingLanguage = new ProgrammingLanguage(id, languageName);
+        programmingLanguageService.add(programmingLanguage);
+        Technology technology = new Technology(id2, technologyName, programmingLanguage);
+        technologyService.add(technology);
 
-
-        Technology spring = new Technology(1, "Spring", java);
-        technologyService.add(spring);
 
         System.out.println("All Programming Languages:");
         for (ProgrammingLanguage pl : programmingLanguageService.getAll()) {
-            System.out.println(pl.getName());
+            System.out.println(pl.getName() + " Id: " + pl.getId());
         }
 
 
         System.out.println("All Technologies Names:");
         for (Technology tech : technologyService.getAll()) {
-            System.out.println(tech.getName());
+            System.out.println(tech.getName() + " Id: " + tech.getId() + " Programming Language: " + tech.getProgrammingLanguage().getName());
         }
     }
 }
